82f6cd2e4eb480b1163a0d68b463aa0ea0ff201f
Draed <draedper@gmail.com>
2017-10-31 23:10:00 +0400

    no name for this commit
    just save, text editor is fucked up

--- src/AppBundle/Services/Mysql.php
+++ src/AppBundle/Services/Mysql.php
@@ -4,18 +4,22 @@ namespace AppBundle\Services;
 
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\LatestUser;
 
 class Mysql{
 
     private $logger;
+    private $doctrine;
     
-    public function __construct( LoggerInterface $logger) {
+    public function __construct( LoggerInterface $logger, EntityManagerInterface $doctrine) {
         $this->logger = $logger;
+        $this->doctrine = $doctrine;
     }
 
     public function GetDataInMysql(){
         //data are already in the database with Fixture Bundle
         //if not then launch $ php bin/console doctrine:fixtures:load
+        $repository = $this->getDoctrine()->getRepository(LatestUser::class);
         $MysqlData = $repository->findAll();
         return $MysqlData;
     }
